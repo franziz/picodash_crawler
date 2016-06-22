4dd2301c11aab90782f6a4e2ef67a2c44d17b9b1
Frans <franssiswanto@gmail.com>
2016-06-22 16:23:26 +0800

    add threading

--- unit_tests.py
+++ unit_tests.py
@@ -1,4 +1,14 @@
-from pymongo import MongoClient
+from pymongo      import MongoClient
+from lib.executor import Runner
+from lib.engine   import Engine
+import _thread
+
+def execute_runner(document=None):
+	assert document is not None, "document is not defined."
+	print(document)
+
+	global current_thread
+	current_thread = current_thread - 1
 
 db        = MongoClient("mongodb://isid:isid123@192.168.1.14/?authSource=admin")
 db        = db.hotp
@@ -18,13 +28,15 @@ for document in db.places.find():
 	))
 #end for
 
+global current_thread
 current_thread = 0
 max_thread     = 10
 for document in documents:
 	while current_thread >= max_thread:
 		time.sleep(random.randint(10/500)/1000)
 
-	print(document)
+	_thread.start_new_thread(execute_runner, (document))
+	current_thread = current_thread + 1
 
 # MOCK_INPUT = {
 # 	     "name":" autogenPointII 1",
